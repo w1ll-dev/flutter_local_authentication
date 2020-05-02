1e15c8b433d0037f8a1f13ff06806d66e2f4bca1
w1ll-dev <w1llmarcio.dev@hotmail.com>
2020-05-02 14:16:35 -0300

    genesis

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'src/auth_page.dart';
+
+void main() => runApp(AuthApp());
+
+class AuthApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Auth App',
+      theme: ThemeData.dark(),
+      home: AuthPage(),
+    );
+  }
+}
